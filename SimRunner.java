9b065ec3f9ddfe83b7594774eb154af19ae1559f
Potatovender <156006555+Potatovender@users.noreply.github.com>
2025-03-22 00:36:22 -0400

    Create SimRunner.java

---
+++ SimRunner.java
@@ -0,0 +1,9 @@
+package AISim2;
+
+//run this file to start the program
+public class SimRunner{
+public static void main(String[] args){
+    Board board=new Board();
+    board.initialize();
+  }
+}
